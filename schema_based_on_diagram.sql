1c68fb4ecf71dd4103ec7f8bbadae42eb78b5424
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-01-18 12:38:13 +0200

    Add: Indices For Tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,6 +4,8 @@ CREATE TABLE patients (
     date_of_birth date
 );
 
+CREATE INDEX patient_id ON patients(id);
+
 CREATE TABLE medical_histories (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     admitted_at timestamp,
@@ -12,12 +14,15 @@ CREATE TABLE medical_histories (
     FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
 
+CREATE INDEX patient_histories_id ON medical_histories(id);
 
 CREATE TABLE treatments(
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     type varchar(40),
     name varchar(40)
 );
+
+CREATE INDEX treatments_id ON treatments(id);
 
 CREATE TABLE invoices (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -27,6 +32,8 @@ CREATE TABLE invoices (
     medical_history_id int,
     FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
 );
+
+CREATE INDEX invoices_id ON invoices(id);
 
 CREATE TABLE invoices_items (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -39,9 +46,17 @@ CREATE TABLE invoices_items (
     FOREIGN KEY(treatment_id) REFERENCES treatments(id)
 );
 
+CREATE INDEX invoices_items_id ON invoices_items(id);
+CREATE INDEX invoice_id ON invoices_items(invoice_id);
+CREATE INDEX treatment_id ON invoices_items(treatment_id);
+
+
 CREATE TABLE history_treatments(
     treatment_id int,
     medical_history_id int,
     FOREIGN KEY(treatment_id) REFERENCES treatments(id),
     FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
 );
+
+CREATE INDEX treatment_id ON history_treatments(treatment_id);
+CREATE INDEX medical_history_id ON history_treatments(medical_history_id);
